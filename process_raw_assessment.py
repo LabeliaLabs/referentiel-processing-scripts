e9c32145b2e4e1017ea74c44d537174559ccc1d1
bowni <bowni@pm.me>
2020-09-03 14:40:49 +0200

    Generates a neat scoring template

--- process_raw_assessment.py
+++ process_raw_assessment.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Parses the DSRC assessment .md file and generates a json file.
-The json file resulting from this script is meant to be directly uploaded in the web platform.
+Parses the DSRC assessment .md file and generates:
+1) a structured .json file, meant to be directly uploaded in the web platform
+2) a neat template for easily adding score points per answer item
 """
 
 import sys
@@ -38,7 +39,6 @@ def main():
         raw = fp.read()
 
         # Capture all sections of the assessment
-        # match_each_sections = re.findall(r'([\#]{3,})\s([Section]+)\s([0-9]+)([\-\s]+)(.+)\n+(.*\n?.*)', raw)  # OLD
         match_each_sections = re.findall(r'[\#]{3,}\s[Section]+\s(?P<section_id>[0-9]+)[\-\s]+(?P<title>.+)\n\n(?P<description>(.|\n)+?)\n\n\[', raw)
         # print(match_each_sections)  # DEBUG
 
@@ -130,9 +130,42 @@ def main():
                 if is_concerned_switch:
                     depends_on = item[0][-1]
 
-    # Generate the .json file
+    # Generate the .json file of the assessment
     with open('assessment.json', 'w', encoding="utf-8") as fp:
         json.dump(dict, fp, ensure_ascii=False, indent=4, separators=(',', ': '), sort_keys=False)
+
+    # Create a neat, easy-to-read and easy-to-write-in scoring template
+
+    txt_lines = []
+    sections_dict = dict["sections"]
+    nb_sections = len(sections_dict)
+
+    for i in range(1, nb_sections+1):
+
+        section_idx = "section " + str(i)
+
+        txt_lines.append(f'# {section_idx} - {sections_dict[section_idx]["name"]}\n')
+        txt_lines.append("\n")
+
+        elements_dict = sections_dict[section_idx]["elements"]
+        nb_elements = len(elements_dict)
+
+        for j in range(1, nb_elements+1):
+
+            element_idx = "element " + str(j)
+
+            txt_lines.append(f'## Q{i}.{j} - {elements_dict[element_idx]["name"]}\n')
+
+            items_dict = elements_dict[element_idx]["answer_items"]
+            for k, v in items_dict.items():
+                txt_lines.append(f'0.00 [{k}] {v["answer_text"]}\n')
+
+            txt_lines.append("\n")
+
+        txt_lines.append("\n")
+
+    with open('scoring_template.txt', 'w', encoding="utf-8") as f:
+        f.writelines(txt_lines)
 
 
 if __name__ == '__main__':
